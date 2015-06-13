f7d2771c4ff34acc8e70436564c6a36760d2f134
TJ Maynes <tjmaynes@outlook.com>
2015-06-13 00:09:38 -0400

    quick update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # wwiki
 getting data from the weather and wikipedia api using sinatra
+
+### Installation
+
+*Create a file called "keystore" in the root directory and put your mashape key on the first line.*
+
+```bash
+bundle install
+ruby server.rb
+```
--- server.rb
+++ server.rb
@@ -12,8 +12,17 @@ end
 
 get '/api/:id' do
   response = Unirest.get "https://community-wikipedia.p.mashape.com/api.php?action=query&format=json&prop=revisions&rvprop=content&titles=#{params['id']}",
-     headers: {
-       "X-Mashape-Key" => key,
-       "Accept" => "application/json"
-     }
+                         headers: {
+                           "X-Mashape-Key" => key,
+                           "Accept" => "application/json"
+                         }
+
+  latitude = response[0]
+  longiture = response[1]
+
+  response = Unirest.get "https://simple-weather.p.mashape.com/weatherdata?lat=" + latitude + "&lng=" + longitude,
+                         headers:{
+                           "X-Mashape-Key" => "l0kmxTytl7mshwOMflAD8VvjnlyOp1nlv2Djsnuc2v2IJ5SRNr",
+                           "Accept" => "application/json"
+                         }
 end
