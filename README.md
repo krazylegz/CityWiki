302c8848bc9ce3e011b22acf20e77b75fd9dcbfd
TJ Maynes <tjmaynes@outlook.com>
2015-06-14 10:55:18 -0400

    geocoder update

--- README.md
+++ README.md
@@ -1,23 +1,14 @@
 # wwiki
-(wip) getting data from the cities gem, weather and wikipedia api using sinatra and mashape
+(wip) getting data from the geocoder gem, weather and wikipedia api using sinatra and mashape
 
 ## Installation
 
 ```bash
-# get json data for every city in the US (required by Cities gem).
-wget https://s3-us-west-2.amazonaws.com/cities-gem/cities.tar.gz
-
-# extract cities from tar file
-tar -xzf cities.tar.gz
-
-# rename cities to data
-mv cities data
-
 # add key from mashape to keystore
 touch keystore
 
 # install dependencies
-bundle install
+bundle
 
 # run server on port 8080
 ruby server.rb
--- server.rb
+++ server.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'json'
 require 'unirest'
-require 'cities'
+require 'geocoder'
 
 set :port, 8080
 key = File.read("keystore")
@@ -13,22 +13,21 @@ end
 get '/:id+:state' do
   content_type :json
 
-  Cities.data_path = './data/'
-  cities = Cities.cities_in_country('US')
-  latitude = cities.find {|city| city["#{params['id']}"].region == "#{params['state']}"}['latitude'].to_i
-  longitude = cities["#{params['id']}"].find {|state| state['region'] == "#{params['state']}"}['longitude'].to_i
-
   wiki_uri = URI.parse(URI.encode("http://en.wikipedia.org/w/api.php?action=query&prop=extracts|info&exintro&titles=#{params['id']}+#{params['state']}&format=json&explaintext&redirects&inprop=url&indexpageids".strip)).to_s
   wiki_response = Unirest.get wiki_uri,
                               headers: {
                                 "X-Mashape-Key" => key,
                                 "Accept" => "application/json"
                               }
-  wiki_data = JSON.parse(wiki_response.body.to_json)
-  wiki_page_id = wiki_data["query"]["pageids"][0]
-  wiki_title = wiki_data["query"]["pages"][wiki_page_id]["title"]
-  wiki_info = wiki_data["query"]["pages"][wiki_page_id]["extract"]
-  wiki_url = wiki_data["query"]["pages"][wiki_page_id]["fullurl"]
+  wiki_data = JSON.parse(wiki_response.body.to_json) || "Wiki data not found from query!"
+  wiki_page_id = wiki_data["query"]["pageids"][0] || "Wiki page id was not found from query!"
+  wiki_title = wiki_data["query"]["pages"][wiki_page_id]["title"] || "Title was not found from query!"
+  wiki_info = wiki_data["query"]["pages"][wiki_page_id]["extract"] || "Info was not found from query!"
+  wiki_url = wiki_data["query"]["pages"][wiki_page_id]["fullurl"] || "Url was not returned from query!"
+
+  city = Geocoder.search("#{params['id']}, #{params['state']}")
+  latitude = city[0].latitude.to_s
+  longitude = city[0].longitude.to_s
 
   weather_uri = "https://simple-weather.p.mashape.com/weatherdata?lat=" + latitude + "&lng=" + longitude
   weather_response = Unirest.get weather_uri,
@@ -37,7 +36,9 @@ get '/:id+:state' do
                                    "Accept" => "application/json"
                                  }
   weather_data = JSON.parse(weather_response.body.to_json)
-  weather_results = weather_data["query"]["results"]
-  puts weather_response.body
-  json = JSON.generate({ :city => wiki_title, :geo => [:latitude => latitude, :longitude => longitude], :info => wiki_info, :url => wiki_url })
+  puts weather_data
+  weather_forecast = weather_data["query"]["results"]["channel"]["item"]["forcast"]
+  puts weather_forecast
+
+  json = JSON.generate({ :city => wiki_title, :geo => [:latitude => latitude, :longitude => longitude], :weather_forcast => weather_forecast, :info => wiki_info, :url => wiki_url })
 end
