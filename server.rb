f1e4d46fde222ec8d10969165ebe68c72bab9b5f
TJ Maynes <tjmaynes@outlook.com>
2015-06-14 02:23:35 -0400

    getting cities/lat/long corrected

--- server.rb
+++ server.rb
@@ -7,19 +7,18 @@ set :port, 8080
 key = File.read("keystore")
 
 get '/' do
-  "citywiki => type http://localhost:8080/tampa as an address url and press enter."
+  "citywiki => type http://localhost:8080/tampa+fl as an address url and press enter."
 end
 
-get '/:id' do
+get '/:id+:state' do
   content_type :json
 
   Cities.data_path = './data/'
   cities = Cities.cities_in_country('US')
-  city = cities["#{params['id']}"]
-  latitude = city.latlong[0].to_s
-  longitude = city.latlong[1].to_s
+  latitude = cities.find {|city| city["#{params['id']}"].region == "#{params['state']}"}['latitude'].to_i
+  longitude = cities["#{params['id']}"].find {|state| state['region'] == "#{params['state']}"}['longitude'].to_i
 
-  wiki_uri = URI.parse(URI.encode("http://en.wikipedia.org/w/api.php?action=query&prop=extracts|info&exintro&titles=#{params['id']}&format=json&explaintext&redirects&inprop=url&indexpageids".strip)).to_s
+  wiki_uri = URI.parse(URI.encode("http://en.wikipedia.org/w/api.php?action=query&prop=extracts|info&exintro&titles=#{params['id']}+#{params['state']}&format=json&explaintext&redirects&inprop=url&indexpageids".strip)).to_s
   wiki_response = Unirest.get wiki_uri,
                               headers: {
                                 "X-Mashape-Key" => key,
@@ -37,6 +36,8 @@ get '/:id' do
                                    "X-Mashape-Key" => key,
                                    "Accept" => "application/json"
                                  }
-  #puts weather_response.body
+  weather_data = JSON.parse(weather_response.body.to_json)
+  weather_results = weather_data["query"]["results"]
+  puts weather_response.body
   json = JSON.generate({ :city => wiki_title, :geo => [:latitude => latitude, :longitude => longitude], :info => wiki_info, :url => wiki_url })
 end
