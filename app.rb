db43121d561a3e410cd5f49f0030ec6c8481a0ef
TJ Maynes <tjmaynes@gmail.com>
2015-11-14 21:38:46 -0500

    Smaller methods calls for wiki and weather data response

--- app.rb
+++ app.rb
@@ -25,23 +25,23 @@ def key
 end
 
 def build_wikipedia
-  wiki = "http://en.wikipedia.org/w/api.php?action=query&prop=extracts|info&exintro&titles="
+  wiki = 'http://en.wikipedia.org/w/api.php?action=query&prop=extracts|info&exintro&titles='
   wiki += "#{@city}+#{@state}&format=json&explaintext&redirects&inprop=url&indexpageids"
 end
 
-def wiki_uri
+def wiki_request
   URI.parse(URI.encode(build_wikipedia.strip)).to_s
 end
 
-def wiki_rest_call
-  Unirest.get wiki_uri,
+def wiki_response
+  Unirest.get wiki_request,
               headers: {
                 "X-Mashape-Key" => key,
                 "Accept" => "application/json"
               }
 end
 
-def wiki_response
+def wiki_data
   JSON.parse(wiki_rest_call.body.to_json) || "Wiki data not found from query!"
 end
 
@@ -57,28 +57,43 @@ def longitude
   city[0].longitude.to_s
 end
 
-def build_weather
+def weather_request
   "https://simple-weather.p.mashape.com/weatherdata?lat=" + latitude + "&lng=" + longitude
 end
 
 def weather_response
-  Unirest.get build_weather,
+  Unirest.get weather_request,
               headers:{
                 'X-Mashape-Key' => key,
                 'Accept' => 'application/json'
               }
 end
 
+def weather_data
+  JSON.parse(weather_response.body.to_json)
+end
+
+def id
+  wiki_data["query"]["pageids"][0] ||= "Wiki Page ID was not found from query!"
+end
+
+def city
+  wiki_data["query"]["pages"][id]["title"] ||= "City was not found from query!"
+end
+
+def info
+  wiki_data["query"]["pages"][id]["extract"] ||= "Info was not found from query!"
+end
+
+def url
+  wiki_data["query"]["pages"][id]["fullurl"] ||= "Url was not returned from query!"
+end
+
+def forecast
+  weather_data["query"]["results"]["channel"]["item"]["forecast"]
+end
+
 def build_json
-  data = wiki_response
-  id = data["query"]["pageids"][0] || "Wiki page id was not found from query!"
-  city = data["query"]["pages"][id]["title"] || "Title was not found from query!"
-  info = data["query"]["pages"][id]["extract"] || "Info was not found from query!"
-  url = data["query"]["pages"][id]["fullurl"] || "Url was not returned from query!"
-
-  weather_data = JSON.parse(weather_response.body.to_json)
-  weather_forecast = weather_data["query"]["results"]["channel"]["item"]["forecast"]
-
   JSON.generate({ :city => city, :geo => [:latitude => latitude, :longitude => longitude],
                   :weather_forcast => weather_forecast, :info => info, :url => url })
 end
