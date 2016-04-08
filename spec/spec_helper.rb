1549402ee967ef7e90e5c8f9e9c03d5817a6adcc
Vikram Dighe <vikram@swiftsignal.com>
2016-04-07 21:39:55 -0400

    Removed Rack testing code from spec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,11 @@
 # spec/spec_helper.rb
-require 'rack/test'
 require 'simplecov'
 SimpleCov.start
 require 'rspec'
 
 ENV['RACK_ENV'] = 'test'
 
-require File.expand_path '../app.rb', __FILE__
-
 module RSpecMixin
-  include Rack::Test::Methods
-
   def app
     Sinatra::Application
   end
