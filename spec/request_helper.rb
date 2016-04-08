935699f7daf6b8361f2a8ea137f22df8032feb23
Vikram Dighe <vikram@swiftsignal.com>
2016-04-08 08:51:43 -0400

    Added configuration for request specs.

---
+++ spec/request_helper.rb
@@ -0,0 +1,18 @@
+require 'simplecov'
+SimpleCov.start
+require 'rack/test'
+require 'rspec'
+
+ENV['RACK_ENV'] = 'test'
+
+require_relative '../app'
+
+module RSpecMixin
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+end
+
+RSpec.configure { |c| c.include RSpecMixin }
