b401a981f441eb12e2b4bf9b39cb3b3c97a50d16
Vikram Dighe <vikram@swiftsignal.com>
2016-04-07 20:45:17 -0400

    Removing unused code.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,8 +14,4 @@ module RSpecMixin
   end
 end
 
-# For RSpec 2.x and 3.x
 RSpec.configure { |c| c.include RSpecMixin }
-
-# If you use RSpec 1.x you should use this instead:
-Spec::Runner.configure { |c| c.include RSpecMixin }
